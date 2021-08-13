836d6d9922f542f7f6fb0919e61e529c3b8744f4
Giulia Campana <giulia@openlattice.com>
2021-08-12 18:50:30 -0700

    show enrollment error

--- chronicle/ContentView.swift
+++ chronicle/ContentView.swift
@@ -50,6 +50,9 @@ struct ContentView: View {
                         .disabled(enrollmentViewModel.enrolling == true)
                     }
                 }
+                if enrollmentViewModel.showEnrollmentError {
+                    Text("Failed to enroll device. Please double check that the Organization ID, Study ID and the Participant ID are correct. If the problem persists, please contact your study administrator.")
+                        .padding(.top)
                 }
                 Spacer()
             }
