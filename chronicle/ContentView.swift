d418043597bb7a2efe1d483ed441ab3bc79a557f
Giulia Campana <giulia@openlattice.com>
2021-08-12 17:59:34 -0700

    put entire page inside a ScrollView for horizontal screen orientation

--- chronicle/ContentView.swift
+++ chronicle/ContentView.swift
@@ -17,62 +17,65 @@ struct ContentView: View {
     let empty = "circle"
 
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Chronicle")
-                .font(.title)
-                .fontWeight(.heavy)
-                .foregroundColor(Color(red: 109/255, green: 73/255, blue: 254/255, opacity: 1.0))
-                .lineLimit(nil)
-            Divider().padding(.bottom)
-            Text("Do you have an organization ID?")
-                .padding(.bottom)
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("Chronicle")
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color(red: 109/255, green: 73/255, blue: 254/255, opacity: 1.0))
+                    .lineLimit(nil)
+                    .padding(.top)
+                Divider().padding(.bottom)
+                Text("Do you have an organization ID?")
+                    .padding(.bottom)
 
-            LazyVGrid(columns: [GridItem(.fixed(30)), GridItem()]) {
-                LazyVGrid(columns: [GridItem()], alignment: .leading) {
-                    Button(action: {
-                        hasOrgId = true
-                    }, label: {
-                        Image(systemName: hasOrgId ? filled : empty)
-                    })
-                    Spacer()
-                    Button(action: {
-                        hasOrgId = false
-                    }, label: {
-                        Image(systemName: !hasOrgId ? filled : empty)
-                    })
-                }
-                LazyVGrid(columns: [GridItem()], alignment: .leading) {
-                    Text("Yes")
-                    Spacer()
-                    Text("No")
-                }
-            }
-            .foregroundColor(Color(red: 109/255, green: 73/255, blue: 254/255, opacity: 1.0))
-
-            if hasOrgId == true {
-                InputFieldView(label: "Enter your Organization ID", inputId: $enrollmentViewModel.organizationId, invalidInput: $enrollmentViewModel.invalidOrganizationId)
-            }
-            InputFieldView(label: "Enter your Study ID", inputId: $enrollmentViewModel.studyId, invalidInput: $enrollmentViewModel.invalidStudyId)
-            InputFieldView(label: "Enter your Participant ID", inputId: $enrollmentViewModel.participantId, invalidInput: $enrollmentViewModel.invalidParticipantId)
-            
-            HStack {
-                Spacer()
-                Button(action: {
-                    Task {
-                        await enrollmentViewModel.enroll()
+                LazyVGrid(columns: [GridItem(.fixed(30)), GridItem()]) {
+                    LazyVGrid(columns: [GridItem()], alignment: .leading) {
+                        Button(action: {
+                            hasOrgId = true
+                        }, label: {
+                            Image(systemName: hasOrgId ? filled : empty)
+                        })
+                        Spacer()
+                        Button(action: {
+                            hasOrgId = false
+                        }, label: {
+                            Image(systemName: !hasOrgId ? filled : empty)
+                        })
                     }
-                }, label: {
-                    Text("Enroll Device")
-                        .foregroundColor(.white)
-                        .padding(10)
-                })
-                .background(Color(red: 109/255, green: 73/255, blue: 254/255, opacity: 1.0))
-                .cornerRadius(8)
+                    LazyVGrid(columns: [GridItem()], alignment: .leading) {
+                        Text("Yes")
+                        Spacer()
+                        Text("No")
+                    }
+                }
+                .foregroundColor(Color(red: 109/255, green: 73/255, blue: 254/255, opacity: 1.0))
+
+                if hasOrgId == true {
+                    InputFieldView(label: "Enter your Organization ID", inputId: $enrollmentViewModel.organizationId, invalidInput: $enrollmentViewModel.invalidOrganizationId)
+                }
+                InputFieldView(label: "Enter your Study ID", inputId: $enrollmentViewModel.studyId, invalidInput: $enrollmentViewModel.invalidStudyId)
+                InputFieldView(label: "Enter your Participant ID", inputId: $enrollmentViewModel.participantId, invalidInput: $enrollmentViewModel.invalidParticipantId)
+                
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        Task {
+                            await enrollmentViewModel.enroll()
+                        }
+                    }, label: {
+                        Text("Enroll Device")
+                            .foregroundColor(.white)
+                            .padding(10)
+                    })
+                    .background(Color(red: 109/255, green: 73/255, blue: 254/255, opacity: 1.0))
+                    .cornerRadius(8)
+                }
+                Spacer()
             }
-            Spacer()
+            .padding(.horizontal)
+            .textFieldStyle(.roundedBorder)
         }
-        .padding(.horizontal)
-        .textFieldStyle(.roundedBorder)
     }
 }
 
