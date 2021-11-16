37203080708f3541812448b66b7cd9f7346129db
Alfonce Nzioka <alfonce.starfish@gmail.com>
2021-11-16 11:41:12 -0800

    register data mock task

--- chronicle/AppDelegate.swift
+++ chronicle/AppDelegate.swift
@@ -7,10 +7,49 @@
 
 import Foundation
 import UIKit
+import BackgroundTasks
+import OSLog
 
 /*
  The app delegate submits task requests and registers launch handlers for database background tasks
  */
 class AppDelegate: NSObject, UIApplicationDelegate {
+    let logger = Logger(subsystem: "com.openlattice.chronicle", category: "AppDelegate")
     
+    let mockDataTaskIdentifer = "com.openlattice.chronicle.mockSensorData"
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        // register handlers for tasks
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: mockDataTaskIdentifer, using: nil) { task in
+            //Downcast parameter to a background refresh task
+            self.handleMockSensorData(task: task as! BGAppRefreshTask)
+        }
+        
+        return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        scheduleMockSensorTask()
+        
+    }
+    
+    func handleMockSensorData(task: BGAppRefreshTask) {
+        // schedule a new task
+        scheduleMockSensorTask()
+        
+        
+    }
+    
+    
+    func scheduleMockSensorTask() {
+        let request = BGAppRefreshTaskRequest(identifier: mockDataTaskIdentifer)
+        request.earliestBeginDate = Date(timeIntervalSinceNow: 15 * 60) // no earlier than 15minutes from now
+        
+        do {
+            try BGTaskScheduler.shared.submit(request)
+        } catch {
+            logger.info("Could not schedule mocking sensor data task: \(error.localizedDescription)")
+        }
+    }
 }
