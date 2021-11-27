238ac657c0a228018d6918a0c104368798f6bb37
Alfonce Nzioka <alfonce.starfish@gmail.com>
2021-11-26 21:17:10 -0800

    update

--- chronicle/AppDelegate.swift
+++ chronicle/AppDelegate.swift
@@ -15,65 +15,65 @@ import OSLog
  */
 class AppDelegate: NSObject, UIApplicationDelegate {
     let logger = Logger(subsystem: "com.openlattice.chronicle", category: "AppDelegate")
-    
+
     // task identifiers in BGTaskSchedulerPermittedIdentifiers array of Info.Plist
     let mockDataTaskIdentifer = "com.openlattice.chronicle.mockSensorData"
-    
+
     var mockDataTaskId: UIBackgroundTaskIdentifier? = nil
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
+
         // register handlers for tasks
         BGTaskScheduler.shared.register(forTaskWithIdentifier: mockDataTaskIdentifer, using: nil) { task in
             //Downcast parameter to a background refresh task
             self.handleMockSensorData(task: task as! BGAppRefreshTask)
         }
-        
+
         return true
     }
-    
+
     func handleMockSensorData(task: BGAppRefreshTask) {
         // schedule a new task
         scheduleMockDataBackgroundTask()
-        
+
         let queue = OperationQueue()
         queue.maxConcurrentOperationCount = 1
-        
+
         guard let context = PersistenceController.shared.newBackgroundContext() else {
             logger.info("unable to execute task")
             task.setTaskCompleted(success: true)
             return
         }
-        
+
         // operation to create fake sensor data and save to database
         let mockDataOperation = MockSensorDataOperation(context: context)
-        
+
         // expiration handler to cancel operation
         task.expirationHandler = {
             queue.cancelAllOperations()
         }
-        
+
         // inform system that task is complete
         mockDataOperation.completionBlock = {
             task.setTaskCompleted(success: !mockDataOperation.isCancelled)
         }
-        
+
         // start the operation
         queue.addOperation(mockDataOperation)
     }
-    
-    // called when app moves to the background to schedule a task to be handled by mockSensorData()
+
+    // called when app moves to the background to schedule a task to be handled by handleMockSensorData()
     func scheduleMockDataBackgroundTask() {
         let request = BGAppRefreshTaskRequest(identifier: mockDataTaskIdentifer)
         request.earliestBeginDate = Date(timeIntervalSinceNow: 15 * 60) // no earlier than 15minutes from now
-        
+
         do {
             try BGTaskScheduler.shared.submit(request)
         } catch {
             logger.info("Could not schedule mocking sensor data task: \(error.localizedDescription)")
         }
     }
-    
+
     // This method is invoked to trigger a single MockSensorData operation.
     @objc func mockSensorData() {
         // create backround context
@@ -81,21 +81,21 @@ class AppDelegate: NSObject, UIApplicationDelegate {
             logger.info("unable to execute task")
             return
         }
-        
+
         // request additional background execution in case app goes to background
         self.mockDataTaskId = UIApplication.shared.beginBackgroundTask(withName: "Create mock sensor data") {
             // end task if time expires
             UIApplication.shared.endBackgroundTask(self.mockDataTaskId!)
             self.mockDataTaskId = UIBackgroundTaskIdentifier.invalid
         }
-        
+
         let mockDataOperation = MockSensorDataOperation(context: context)
         mockDataOperation.completionBlock = {
             // end task after operation is completed
             UIApplication.shared.endBackgroundTask(self.mockDataTaskId!)
             self.mockDataTaskId = UIBackgroundTaskIdentifier.invalid
         }
-        
+
         mockDataOperation.start()
     }
 }
