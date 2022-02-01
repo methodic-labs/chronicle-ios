a724e686bd2eea60dd70f5d4b4d91e2891166210
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-02-01 03:34:36 -0800

    test importing to core data

---
+++ chronicle/Utilities/TestUtils.swift
@@ -0,0 +1,103 @@
+//
+//  TestUtils.swift
+//  chronicleTests
+//
+//  Created by Alfonce Nzioka on 2/1/22.
+//  Copyright © 2022 OpenLattice, Inc. All rights reserved.
+//
+
+import Foundation
+import SensorKit
+
+
+// util methods for unit tests
+
+struct TestUtils {
+    // Generating Random String
+    private static func randomString(length: Int) -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyz"
+        return String((0..<length).map{ _ in letters.randomElement()! })
+    }
+    
+    static func mockDeviceUsageData() -> Data? {
+        let data = DeviceUsageDataSample(
+            totalScreenWakes: 30,
+            totalUnlocks: 20,
+            totalUnlockDuration: 235.9,
+            appUsage: [
+                "books": [
+                    AppUsage(
+                        usageTime: 223.9,
+                        textInputSessions: ["keyboard": 89, "pencil": 423],
+                        bundleIdentifer: randomString(length: 5))]
+            ],
+            webUsage: ["catalogs": 9202.0],
+            notificationUsage: [
+                "education": [
+                    NotificationUsage(bundleIdentifier: randomString(length: 9), event: "clear"),
+                    NotificationUsage(bundleIdentifier: randomString(length: 7), event: "expired")
+                ],
+                "finance": [
+                    NotificationUsage(bundleIdentifier: randomString(length: 4), event: "hide")
+                ]
+            ]
+        )
+        
+        return try? JSONEncoder().encode(data)
+    }
+    
+    private static func mockKeyboardMetricsData() -> Data? {
+        let data = KeyboardMetricsDataSample(
+            totalWords: 34,
+            totalAlteredWords: 98,
+            totalTaps: 93,
+            totalDrags: 379,
+            totalDeletes: 392,
+            totalEmojis: 90,
+            totalSpaceCorrections: 0,
+            totalTypingDuration: 0,
+            totalHitTestCorrections: 0,
+            totalSubstitutionCorrections: 893,
+            totalNearKeyCorrections: 039,
+            totalSkipTouchCorrections: 0920,
+            totalInsertKeyCorrections: 20,
+            totalTranspositionCorrections: 2,
+            totalRetroCorrections: 89,
+            totalAutoCorrections: 9,
+            totalPaths: 90,
+            totalPathTime: 92,
+            emojiCountBySentiment: ["angry": 45, "happy": 77],
+            wordCountBySentiment: ["down": 89, "excited": 93]
+        )
+        
+        return try? JSONEncoder().encode(data)
+    }
+    
+    private static func mockMessagesUsageData() -> Data? {
+        let data = MessagesUsageDataSample(totalIncomingMessages: 34, totalOutgoingMessages: 98, totalUniqueContacts: 4)
+        return try? JSONEncoder().encode(data)
+    }
+    
+    private static func mockPhoneUsageData() -> Data? {
+        let data = PhoneUsageDataSample(totalIncomingCalls: 3, totalOutgoingCalls: 10, totalPhoneDuration: 600.9, totalUniqueContacts: 5)
+        
+        return try? JSONEncoder().encode(data)
+    }
+    
+    static func mockSensorDataSample(sensor: Sensor) -> SensorDataProperties {
+        var data: Data?
+        
+        switch (sensor) {
+        case .keyboardMetrics:
+            data = mockKeyboardMetricsData()
+        case .deviceUsage:
+            data = mockDeviceUsageData()
+        case .messagesUsage:
+            data = mockMessagesUsageData()
+        case .phoneUsage:
+            data = mockPhoneUsageData()
+        }
+        
+        return SensorDataProperties(sensor: sensor, duration: 2423.9, writeTimeStamp: SRAbsoluteTime(rawValue: 15413910.591), data: data)
+    }
+}
--- chronicleTests/CoreDataTests.swift
+++ chronicleTests/CoreDataTests.swift
@@ -7,30 +7,45 @@
 //
 
 import XCTest
+import CoreData
+@testable import chronicle
 
 class CoreDataTests: XCTestCase {
 
+    var context: NSManagedObjectContext?
+    let appDelegate = AppDelegate()
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let container = PersistenceController.preview.persistentContainer
+        guard let container = container else {
+            fatalError("unable to set up core data stack")
+        }
+        context = container.viewContext
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testStoreDeviceUsageDataSample() {
+        guard let context = context else {
+            return
+        }
+        
+        let data = TestUtils.mockSensorDataSample(sensor: Sensor.deviceUsage)
+        XCTAssertTrue(data.isValidSample)
+        
+        let operation = ImportIntoCoreDataOperation(context: context, data: data)
+        
+        operation.completionBlock = {
+      
+            let fetchRequest: NSFetchRequest<SensorData> = SensorData.fetchRequest()
+            let objects = try? context.fetch(fetchRequest)
+            XCTAssertNotNil(objects)
+            XCTAssertTrue(objects!.count == 1)
+            
+            
+            
         }
     }
-
 }
