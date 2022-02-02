86b9c86f9255dd77364f1f0a2dc0ce0e60cace94
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-02-02 14:43:49 -0800

    update enroll path

--- chronicle/Utilities/ApiUtils.swift
+++ chronicle/Utilities/ApiUtils.swift
@@ -14,10 +14,11 @@ struct ApiUtils {
     static var host = "api.openlattice.com"
     static let v2Base = "/chronicle/v2"
     static let base = "/chronicle"
-
+    
     // path constants
     static let enrollPath = "enroll"
-    static let studyPath = "study"
+    static let study = "study"
+    static let participant = "participant"
     static let edmPath = "edm"
 
     // returns an optional URLComponent with orgId in the path
@@ -33,7 +34,7 @@ struct ApiUtils {
         var components = URLComponents()
         components.scheme = scheme
         components.host = host
-        components.path = "\(v2Base)/\(enrollment.organizationId!)/\(enrollment.studyId!)/\(enrollment.participantId)/\(deviceId)/\(enrollPath)"
+        components.path = "\(base)/\(study)/\(enrollment.studyId!)/\(participant)/\(enrollment.participantId)/\(deviceId)/\(enrollPath)"
 
         // debug: set components.scheme = 'http', components.host = [local server ip] , components.port = 8090
         // expected path: /chronicle/v2 + ORGANIZATION_ID_PATH + STUDY_ID_PATH + PARTICIPANT_ID_PATH + DATASOURCE_ID_PATH + ENROLL_PATH
